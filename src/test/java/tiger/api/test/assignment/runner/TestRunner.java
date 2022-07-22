384d8f96038aca16dd3a5e70fc12e8d6c6ddf35c
Farshaid Rafi <Farshaid.rafi@gmail.com>
2022-07-21 20:39:51 -0400

    Assignment

---
+++ src/test/java/tiger/api/test/assignment/runner/TestRunner.java
@@ -0,0 +1,13 @@
+package tiger.api.test.assignment.runner;
+
+import com.intuit.karate.junit5.Karate;
+
+public class TestRunner {
+	@Karate.Test
+	public Karate runTest() {
+		return Karate.run("classpath:Features")
+				.tags("smoke");
+	}
+}
+
+//gh
